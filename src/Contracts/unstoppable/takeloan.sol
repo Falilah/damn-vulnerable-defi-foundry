5c77ce02ad7e4a5b737ba77d8777b89fcbccae03
faliah <leogoldf21@gmail.com>
2023-01-10 12:50:40 +0100

    cracked unstoppable

---
+++ src/Contracts/unstoppable/takeloan.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {UnstoppableLender} from "./UnstoppableLender.sol";
+
+contract TakeLoan {
+    address lender;
+    // constructor(address Ilender) {
+    //     Unstoppable(lender) = Unstoppable(Ilender);
+    // }
+
+    // function TakeLoan(uint amount) public{
+
+    // }
+}
